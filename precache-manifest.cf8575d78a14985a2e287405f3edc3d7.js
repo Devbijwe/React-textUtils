78c229ac300871b3d9487920bef8b3cb29b41ffb
Devbijwe <95902248+Devbijwe@users.noreply.github.com>
2022-12-26 19:07:25 +0530

    Updates

---
+++ precache-manifest.cf8575d78a14985a2e287405f3edc3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ec839d7b6380931b804",
+    "url": "/React-textUtils/static/css/main.317a8e60.chunk.css"
+  },
+  {
+    "revision": "7ec839d7b6380931b804",
+    "url": "/React-textUtils/static/js/main.7ec839d7.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/React-textUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/React-textUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "6d8d72df2100ec518857",
+    "url": "/React-textUtils/static/js/runtime~main.6d8d72df.js"
+  },
+  {
+    "revision": "850758505bc61963d318ee5bbeecf0e1",
+    "url": "/React-textUtils/index.html"
+  }
+];
